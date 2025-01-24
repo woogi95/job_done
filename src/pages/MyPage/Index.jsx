04c5383d9b229e0591e1bba44113ffcfba9c3492
blendy9702 <blendy9702@gmail.com>
2025-01-24 14:54:56 +0900

    [feat] 마이페이지 레이아웃 작업

--- src/pages/MyPage/Index.jsx
+++ src/pages/MyPage/Index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MyPageLayout from "../../components/MyPageLayout";
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <MyPageLayout>마이페이지</MyPageLayout>
+    </div>
+  );
 }
 
 export default MyPage;
